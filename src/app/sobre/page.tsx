4d75c9114cb2299bcdc546d308914f08d8fa844e
Ricardo Lemos <ricardolemos@MacBook-Air-de-Ricardo-3.local>
2024-05-08 22:38:50 -0300

    create pillars section

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,14 +1,16 @@
 import { Sobre } from "@/types/sobre";
 import fetchDataFromApi from "@/helpers/fetchFromApi";
 import AboutBanner from "../../components/about/banner";
+import AboutPillars from "../../components/about/pillars";
 
 export default async function Page() {
   const data = await fetchDataFromApi<Sobre>("sobre");
-  const { inicial } = data.attributes;
+  const { inicial, Pilar } = data.attributes;
 
   return (
     <>
       <AboutBanner inicial={inicial}></AboutBanner>
+      <AboutPillars pillars={Pilar}></AboutPillars>
     </>
   );
 }
---
+++ src/components/about/pillars.tsx
@@ -0,0 +1,38 @@
+import { Pilares } from "@/types/components";
+import ApiImage from "../ApiImage";
+
+export default function AboutPillars({
+  pillars,
+}: {
+  readonly pillars: Pilares[];
+}) {
+  const imgStyles = "h-40 w-40 object-contain object-center";
+  return (
+    <section className="flex flex-col items-center py-12">
+      <div className="container flex flex-col items-center gap-8">
+        <h2 className="text-3xl font-bold text-primary">Pilares da Empresa</h2>
+        <div className="grid-lines-3 grid grid-cols-3 justify-center gap-10">
+          {pillars?.map((pillar, i) => (
+            <div
+              className="flex w-72 flex-col items-center justify-start gap-5"
+              key={"aboutPillar" + i}
+            >
+              <div className="">
+                <ApiImage
+                  image={pillar?.Imagem?.data}
+                  contentStyles={imgStyles}
+                />
+              </div>
+              <div className="flex h-20 items-center">
+                <p className="text-center text-3xl font-bold text-primary">
+                  {pillar?.Titulo}
+                </p>
+              </div>
+              <p className=" text-center text-black">{pillar?.Corpo}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
