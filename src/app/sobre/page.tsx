1c94fc57e8de9b93fa21d3e81bdd01c74b19e909
Ricardo Lemos <ricardolemos@MacBook-Air-de-Ricardo-3.local>
2024-05-12 16:53:34 -0300

    create about reselas section

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,15 +2,20 @@ import { Sobre } from "@/types/sobre";
 import fetchDataFromApi from "@/helpers/fetchFromApi";
 import AboutBanner from "../../components/about/banner";
 import AboutPillars from "../../components/about/pillars";
+import AboutResales from "@/components/about/resales";
+import { Revenda } from "@/types/revenda";
 
 export default async function Page() {
   const data = await fetchDataFromApi<Sobre>("sobre");
-  const { inicial, Pilar } = data.attributes;
+  const { inicial, Pilar, Revendas } = data.attributes;
 
   return (
     <>
       <AboutBanner inicial={inicial}></AboutBanner>
       <AboutPillars pillars={Pilar}></AboutPillars>
+      {Revendas != undefined && (
+        <AboutResales resales={Revendas}></AboutResales>
+      )}
     </>
   );
 }
---
+++ src/components/about/resales.tsx
@@ -0,0 +1,39 @@
+import { Revendas } from "@/types/components";
+import ApiImage from "../ApiImage";
+import Image from "next/image";
+import separator from "../../../public/separator_white.svg";
+
+export default function AboutResales({
+  resales,
+}: {
+  readonly resales: Revendas;
+}) {
+  const imgStyles = "w-96 h-auto";
+  const separatorStyle = "w-12";
+  const { Titulo, Foto, Corpo } = resales;
+  return (
+    <section className="bg-primary p-10">
+      <div className="container">
+        <div className="w- flex flex-row gap-8">
+          <ApiImage contentStyles={imgStyles} image={Foto.data}></ApiImage>
+          <div className="flex flex-col justify-center gap-8">
+            <Image
+              src={separator}
+              alt="separador"
+              className={separatorStyle}
+            ></Image>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-3xl font-bold text-white">{Titulo}</h2>
+              <p className="text-white">{Corpo}</p>
+            </div>
+            <Image
+              src={separator}
+              alt="separador"
+              className={separatorStyle}
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
