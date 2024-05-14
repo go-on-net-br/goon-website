4309c939ae4354310646bff80c4fe1b1fa5962cd
Rick Lemos <ricardocamargolemos@gmail.com>
2024-05-13 22:54:11 -0300

    create information section

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -5,10 +5,11 @@ import AboutPillars from "../../components/about/pillars";
 import AboutResales from "@/components/about/resales";
 import { Revenda } from "@/types/revenda";
 import AboutMVV from "@/components/about/mvv";
+import AboutInformation from "@/components/about/information";
 
 export default async function Page() {
   const data = await fetchDataFromApi<Sobre>("sobre");
-  const { inicial, Pilar, Revendas, Card } = data.attributes;
+  const { inicial, Pilar, Revendas, Card, Informacoes } = data.attributes;
 
   return (
     <>
@@ -17,6 +18,7 @@ export default async function Page() {
       <AboutResales resales={Revendas}></AboutResales>
       <AboutResales resales={Revendas}></AboutResales>
       <AboutMVV mvv={Card}></AboutMVV>
+      <AboutInformation information={Informacoes}></AboutInformation>
     </>
   );
 }
