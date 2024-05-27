95e4a47da250d76a550d962e6edaf82d997ef542
Guilherme Almeida <johndoe@example.com>
2024-05-27 20:53:19 -0300

    fix: improme semantic html use

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -35,42 +35,44 @@ export default async function Projetos() {
   ];
   return (
     <>
-      <BlueBgBox
-        bgImage="/blueprint.webp"
-        boxStyles="w-screen h-[550px] after:!opacity-[36%]"
-      >
-        <div className="mx-auto mt-20 w-[700px] text-center text-white">
-          <h1 className="mb-4 text-6xl font-bold">Projetos Referência</h1>
-          <p className="text-lg font-light">
-            Aqui reunimos projetos incríveis de nossas empresas parceiras para
-            que você conheça mais a fundo o que a sua casa inteligente pode
-            fazer por você
-          </p>
-        </div>
-        <div className="container absolute left-0 right-0 mx-auto flex w-full max-w-screen-xl justify-evenly gap-6 pt-32 text-center text-white">
-          {cards.map((card) => {
-            return (
-              <div
-                className="card w-80 rounded-3xl bg-primary shadow-xl"
-                key={card.title}
-              >
-                <figure className="px-8 pt-8">
-                  <Image src={card.src} alt={card.title} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title mx-auto mb-2 text-2xl font-medium leading-6">
-                    {card.title}
-                  </h2>
-                  <p>{card.description}</p>
+      <section>
+        <BlueBgBox
+          bgImage="/blueprint.webp"
+          boxStyles="w-screen h-[550px] after:!opacity-[36%]"
+        >
+          <div className="mx-auto mt-20 w-[700px] text-center text-white">
+            <h1 className="mb-4 text-6xl font-bold">Projetos Referência</h1>
+            <p className="text-lg font-light">
+              Aqui reunimos projetos incríveis de nossas empresas parceiras para
+              que você conheça mais a fundo o que a sua casa inteligente pode
+              fazer por você
+            </p>
+          </div>
+          <div className="container absolute left-0 right-0 mx-auto flex w-full max-w-screen-xl justify-evenly gap-6 pt-32 text-center text-white">
+            {cards.map((card) => {
+              return (
+                <div
+                  className="card w-80 rounded-3xl bg-primary shadow-xl"
+                  key={card.title}
+                >
+                  <figure className="px-8 pt-8">
+                    <Image src={card.src} alt={card.title} />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title mx-auto mb-2 text-2xl font-medium leading-6">
+                      {card.title}
+                    </h2>
+                    <p>{card.description}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      </BlueBgBox>
-      <div className="container relative z-[1] mx-auto -mt-10 max-w-screen-xl rounded-3xl bg-white px-9 py-64">
+              );
+            })}
+          </div>
+        </BlueBgBox>
+      </section>
+      <section className="container relative z-[1] mx-auto -mt-10 max-w-screen-xl rounded-3xl bg-white px-9 py-64">
         <ProjectListing projects={projectsData} />
-      </div>
+      </section>
       <GradientFooter bgImage="/blueprint_zoom.webp" boxStyles="text-3xl">
         <div className="box-border flex h-full w-[820px] flex-col justify-center pl-9 text-white">
           <Image
