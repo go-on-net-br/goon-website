a35b0ccc21753f884f34a170f07f8f9cde735f0c
Guilherme Almeida <johndoe@example.com>
2024-05-27 21:02:48 -0300

    feat: create brands header

---
+++ src/app/marcas/page.tsx
@@ -0,0 +1,18 @@
+export default function Marcas() {
+  return (
+    <div className="container mx-auto max-w-screen-xl">
+      <header className="mx-auto my-20 max-w-[890px] text-center text-primary">
+        <h1 className="w-fit text-5xl font-bold uppercase ">
+          Distribuidores exclusivos{" "}
+          <span className="font-normal normal-case">das</span>
+          <br /> melhores marcas do mercado
+        </h1>
+        <p className="mt-6 text-xl font-light">
+          Rodamos o mundo buscando as melhores empresas, com credibilidade,
+          qualidade e produtos inovadores para tornar a casa dos brasileiros
+          ainda mais inteligente e funcional.
+        </p>
+      </header>
+    </div>
+  );
+}
