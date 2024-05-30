d2d03dcdfb67c9a1c4017d1fcad0b90b4a38b1ea
Rick Lemos <ricardocamargolemos@gmail.com>
2024-05-29 23:22:59 -0300

    fix product carousel buttons

--- src/components/products/carousel.tsx
+++ src/components/products/carousel.tsx
@@ -2,6 +2,8 @@ import { Produto } from "@/types/produto";
 import ApiImage from "../ApiImage";
 import { useState } from "react";
 import { Media } from "@/types/common";
+import arrow_left from "../../../public/arrow_left.svg";
+import Image from "next/image";
 
 export default function ProductsCarousel({ media }: { media: Media[] }) {
   const [page, setPage] = useState(0);
@@ -17,24 +19,22 @@ export default function ProductsCarousel({ media }: { media: Media[] }) {
       <div className="carousel flex h-full w-full flex-col justify-center">
         <ApiImage contentStyles="object-contain py-12" image={media[page]} />
         <div className="absolute bottom-2 left-1/2 flex -translate-x-1/2 transform items-center gap-2 align-text-bottom text-xl text-primary">
-          <button
-            onClick={() => changePage(page - 1)}
-            className={page === 0 ? "text-goOnGrey" : "hover:cursor-pointer"}
-          >
-            {"< "}
+          <button onClick={() => changePage(page - 1)}>
+            <Image
+              className=" h-4 w-4 object-contain"
+              alt="flecha apontada para esquerda"
+              src={arrow_left}
+            />
           </button>
           <span className="text-base font-light">
             {page + 1} de {media.length}
           </span>
-          <button
-            onClick={() => changePage(page + 1)}
-            className={
-              page === media.length - 1
-                ? "text-goOnGrey"
-                : "hover:cursor-pointer"
-            }
-          >
-            {" >"}
+          <button onClick={() => changePage(page + 1)}>
+            <Image
+              className=" h-4 w-4 scale-x-[-1] object-contain"
+              alt="flecha apontada para esquerda"
+              src={arrow_left}
+            />
           </button>
         </div>
       </div>
