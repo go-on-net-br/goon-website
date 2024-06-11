8d1cc618109455b8301be446cb42ca55466241cf
Rick Lemos <ricardocamargolemos@gmail.com>
2024-06-11 12:02:54 -0300

    apply BlueBgBox to products header

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,33 +1,31 @@
 import { Produto } from "@/types/produto";
-import Image from "next/image";
-import blueprintImage from "../../../public/blueprint.webp";
 import fetchDataFromApi from "@/helpers/fetchFromApi";
 import ProductsList from "@/components/products/list";
 import BeAReseller from "@/components/beAResellerFooter";
 import { Metadata } from "next";
-
+import BlueBgBox from "@/components/blueBgBox";
 
 export const metadata: Metadata = {
-  title:'Produtos',
+  title: "Produtos",
 };
 
 export default async function Page() {
   const productsFromApi = await fetchDataFromApi<Produto[]>("produtos");
   return (
     <>
-      <section className="absolute z-[-1] max-h-72 bg-primary">
-        <Image
-          className="max-h-72 min-h-56 object-cover opacity-25"
-          alt="mãos apontando para uma folha de projeto"
-          src={blueprintImage}
-        />
-      </section>
-      <section className="flex flex-col items-center px-4 py-16">
-        <h1 className=" text-4xl font-bold text-white md:text-5xl">Produtos</h1>
-        <p className=" max-w-xl text-center text-white">
-          Oferecemos uma gama completa de produtos, das melhores marcas que irão
-          elevar a experiência do seu projeto
-        </p>
+      <section>
+        <BlueBgBox
+          bgImage="/blueprint.webp"
+          boxStyles="w-screen h-[300px] after:!opacity-[36%]"
+        >
+          <div className="mx-auto mt-20 text-center text-white md:w-[700px]">
+            <h1 className="mb-4 text-4xl font-bold md:text-6xl">Produtos</h1>
+            <p className="text-lg font-light">
+              Oferecemos uma gama completa de produtos, das melhores marcas que
+              irão elevar a experiência do seu projeto
+            </p>
+          </div>
+        </BlueBgBox>
       </section>
       <section className=" -mt-4 flex flex-col items-center">
         <ProductsList productsFromApi={productsFromApi} />
