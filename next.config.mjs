a7de557ec3f3b3857d2658e8a77583998a3d5463
Guilherme Almeida <johndoe@example.com>
2024-06-13 22:31:58 -0300

    fix: add statixc deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
+// @ts-check
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: isDevelopment ? "standalone" : "export",
   images: {
+    unoptimized: !isDevelopment,
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "127.0.0.1",
+        hostname: "http://104.131.160.214",
         port: "1337",
         pathname: "/uploads/**",
       },
     ],
   },
+  staticPageGenerationTimeout: 10000,
 };
 
 export default nextConfig;
