556f6fd74099b2294fc388ab0d6eb454c9ffb081
Guilherme Almeida <guica.almeida@gmail.com>
2024-08-24 12:44:02 -0300

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,30 +5,32 @@ import SocialNetworks from "./socialNetworks";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center bg-primary p-12 text-primary-content">
-      <div className="flex flex-col md:flex-row">
-        <aside>
+    <footer className="footer footer-center bg-primary p-8 text-primary-content md:p-12">
+      <div className="flex md:flex-row">
+        <aside className="w-[49%] md:w-auto">
           <Link href="/">
             <Image
               src={goOnSvg}
-              className="mt-2 h-16 w-fit object-contain"
+              className="h-16 w-fit object-contain md:mt-2"
               alt="GoOn logo"
             />
           </Link>
         </aside>
-        <div className="mx-4 my-4 h-0.5 w-full rounded-full bg-white md:h-20 md:w-1" />
-        <nav>
+        <div className="mx-2 my-auto h-12 w-1 rounded-full bg-white md:mx-4 md:my-4 md:h-20" />
+        <nav className="w-[49%] md:w-auto">
           <p className="mb-3 text-left font-bold text-white">
             Nos encontre aqui
           </p>
-          <div className="grid grid-flow-col gap-6">
-            <SocialNetworks iconStyle="h-10 w-10" />
+          <div className="grid grid-flow-col gap-2 md:gap-6">
+            <SocialNetworks iconStyle="h-5 w-5" />
           </div>
         </nav>
       </div>
-      <p className="tracking-wider text-white">
-        (11) 4328-8808 | contato@goon.net.br
-      </p>
+      <div className="flex max-w-96 w-full justify-evenly tracking-wider text-white">
+        <p>+55 (11) 4328-8808</p>
+        <span> | </span>
+        <span>contato@goon.net.br</span>
+      </div>
     </footer>
   );
 }
