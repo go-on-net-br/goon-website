406bf116f0bdb4534fa00f9c95246bf36707c4f1
Rick Lemos <ricardocamargolemos@gmail.com>
2024-09-17 01:50:09 -0300

    fix mobile contact screen bottom

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 function TalkToUsForm() {
   return (
     <div className="w-full rounded-md bg-white p-8 shadow-2xl xl:absolute xl:-top-[175px] xl:right-0 xl:w-[712px] xl:rounded-none">
-      <h1 className=" mb-6 text-3xl font-bold uppercase text-primary xl:text-5xl">
+      <h1 className=" mb-6 hidden text-3xl font-bold uppercase text-primary xl:block xl:text-5xl">
         Fale conosco
       </h1>
       <div className="text-lg">
@@ -25,6 +25,9 @@ function TalkToUsForm() {
 function ContactInfo() {
   return (
     <div className="flex flex-col items-center gap-8 py-10 xl:w-2/5 xl:items-start xl:py-0">
+      <p className="text-3xl font-bold uppercase text-white xl:hidden">
+        Fale conosco
+      </p>
       <div className="flex items-center gap-4 tracking-wider text-white">
         <Image
           src={phoneIcon}
@@ -58,7 +61,7 @@ export default function Contato() {
         </section>
       </div>
       <div className="mx-8 flex w-full flex-col items-center xl:hidden">
-        <section className="mx-4 h-full w-full bg-primary sm:w-3/5">
+        <section className="mx-4 h-[950px] w-full max-w-[570px] bg-primary">
           <ContactInfo />
           <div className="absolute left-0 w-screen px-4">
             <TalkToUsForm />
