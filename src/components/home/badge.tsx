8a3e9b9db8c53b7f32be576c41050e1a6cc7f529
Guilherme Almeida <guica.almeida@gmail.com>
2024-12-23 19:23:14 -0300

    fix: badge props

--- src/components/home/badge.tsx
+++ src/components/home/badge.tsx
@@ -1,37 +1,43 @@
 import Image from "next/image";
 
+type BadgeTextSize = "sm" | "md" | "lg";
+export type BadgeProps = { src: any; text: string; fontSize: BadgeTextSize };
+
 export default function Badge({
   badges,
   white = false,
 }: {
-  badges: { src: any; text: string; fontSize: string }[];
+  badges: BadgeProps[];
   white?: boolean;
 }) {
   return (
-    <div className="mx-auto grid w-full grid-cols-2 md:grid-cols-3 gap-2">
+    <div className="mx-auto grid w-full grid-cols-2 gap-2 md:grid-cols-3">
       {badges?.map((badge, i) => {
         let fontSize = "text-sm md:text-xl ";
         if (badge.fontSize === "md") {
           fontSize = "text-xs md:text-lg ";
         }
         if (badge.fontSize === "sm") {
-          fontSize = "text-xxs md:text-md ";
+          fontSize = "text-[10px] md:text-md ";
         }
 
         const badgeText = badge.text.split("\n");
         return (
           <div
-            className={"mx-auto flex w-80 gap-2 " + (i === badges.length - 1 ? "col-span-2 md:col-span-1 " : "")}
+            className={
+              "mx-auto flex w-80 gap-2 " +
+              (i === badges.length - 1 ? "col-span-2 md:col-span-1 " : "")
+            }
             key={badge?.text.slice(0, 5) + i}
           >
-            <div className="h-14 md:h-16 md:w-24 p-2">
+            <div className="h-14 p-2 md:h-16 md:w-24">
               <Image
                 src={badge?.src}
                 alt={badge?.text}
                 className="ml-auto h-full w-fit object-contain object-left"
               />
             </div>
-            <p className="flex flex-col uppercase tracking-widest justify-center">
+            <p className="flex flex-col justify-center uppercase tracking-widest">
               {badgeText.map((txt, txtI) => {
                 return (
                   <span
