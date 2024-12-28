bf436fc4cf106daf6c7d561e69e0640486b78352
Rick Lemos <ricardocamargolemos@gmail.com>
2024-12-27 21:13:58 -0300

    fix: brand page upbated

--- src/app/marcas/page.tsx
+++ src/app/marcas/page.tsx
@@ -1,4 +1,5 @@
 import ApiImage from "@/components/ApiImage";
+import BlueBgBox from "@/components/blueBgBox";
 import fetchDataFromApi from "@/helpers/fetchFromApi";
 import { buildMediaQPs } from "@/helpers/qpHelper";
 import universalSlugify from "@/helpers/universalSlugify";
@@ -18,54 +19,57 @@ export default async function Marcas() {
   );
 
   return (
-    <div className="container mx-auto my-10 max-w-screen-xl px-8 md:my-20">
-      <header className="mx-auto my-10 max-w-[890px] text-center text-primary md:my-20">
-        <h1 className="w-full text-2xl font-bold uppercase md:w-fit md:text-5xl ">
-          <span className="text-base md:text-5xl">
-            Distribuidores exclusivos das
-          </span>{" "}
-          melhores marcas do mercado
-        </h1>
-        <p className="mt-6 text-sm font-light md:text-xl">
-          Rodamos o mundo buscando as melhores empresas, com credibilidade,
-          qualidade e produtos inovadores para tornar a casa dos brasileiros
-          ainda mais inteligente e funcional.
-        </p>
-      </header>
-      <section className="align-center mx-auto flex flex-wrap justify-center gap-8">
-        {brandsData?.map((brand) => {
-          const { Capa, Logotipo, Marca, Resumo } = brand.attributes;
-
-          return (
-            <Link
-              href={"marcas/" + universalSlugify(Marca)}
-              className="card w-96 cursor-pointer bg-base-100 shadow-xl transition-all hover:scale-105"
-              key={Marca}
-            >
-              <figure>
-                <div className="flex h-72 w-full items-center justify-center overflow-hidden">
-                  <ApiImage
-                    image={Capa.data}
-                    contentStyles="min-w-full min-h-full object-cover"
-                  />
-                </div>
-              </figure>
-              <div className="card-body">
-                <div className="relative mx-auto w-fit">
-                  <h2 className="absolute bottom-0 left-0 right-0 top-0 z-0 block">
-                    {Marca}
-                  </h2>
-                  <ApiImage
-                    image={Logotipo.data}
-                    contentStyles="relative top-0 left-0 z-10 bg-white h-20 object-contain"
-                  />
-                </div>
-                <p className="pt-2 text-sm md:text-base">{Resumo}</p>
-              </div>
-            </Link>
-          );
-        })}
+    <>
+      <section>
+        <BlueBgBox
+          bgImage="/suburbamNeighborhood.webp"
+          boxStyles="w-screen h-[250px] md:pb-0 md:h-[350px] after:!opacity-[25%]"
+        >
+          <div className="mx-2 mt-8 text-center text-white md:mx-auto md:mt-20 md:w-[700px]">
+            <h1 className="mb-4 text-4xl font-bold md:text-5xl">Marcas</h1>
+            <p className="text-lg font-light md:text-xl">
+              Rodamos o mundo buscando as melhores empresas, com credibilidade,
+              qualidade e produtos inovadores para tornar a casa dos brasileiros
+              ainda mais inteligente e funcional.
+            </p>
+          </div>
+        </BlueBgBox>
       </section>
-    </div>
+      <section className="container relative z-[1] mx-auto max-w-screen-xl rounded-3xl bg-white px-8 pb-12 pt-8 md:-mt-16 md:px-0 md:py-12 md:pt-24">
+        <div className="align-center flex flex-wrap justify-around gap-8 md:gap-16">
+          {brandsData?.map((brand) => {
+            const { Capa, Logotipo, Marca, Resumo } = brand.attributes;
+            return (
+              <Link
+                href={"marcas/" + universalSlugify(Marca)}
+                className="card w-96 cursor-pointer bg-base-100 shadow-xl transition-all hover:scale-105"
+                key={Marca}
+              >
+                <figure>
+                  <div className="flex h-72 w-full items-center justify-center overflow-hidden">
+                    <ApiImage
+                      image={Capa.data}
+                      contentStyles="min-w-full min-h-full object-cover"
+                    />
+                  </div>
+                </figure>
+                <div className="card-body">
+                  <div className="relative mx-auto w-fit">
+                    <h2 className="absolute bottom-0 left-0 right-0 top-0 z-0 block">
+                      {Marca}
+                    </h2>
+                    <ApiImage
+                      image={Logotipo.data}
+                      contentStyles="relative top-0 left-0 z-10 bg-white h-20 object-contain"
+                    />
+                  </div>
+                  <p className="pt-2 text-sm md:text-base">{Resumo}</p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
